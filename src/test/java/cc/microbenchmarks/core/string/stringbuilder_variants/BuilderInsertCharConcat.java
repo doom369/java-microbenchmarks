4974d28fd4dabb96cbadf9ca55d928e199c11671
Dmitriy Dumanskiy <doom369@gmail.com>
2017-06-14 17:31:23 +0300

    StringBuilder insert

---
+++ src/test/java/cc/microbenchmarks/core/string/stringbuilder_variants/BuilderInsertCharConcat.java
@@ -0,0 +1,76 @@
+package cc.microbenchmarks.core.string.stringbuilder_variants;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * The Blynk Project.
+ * Created by Dmitriy Dumanskiy.
+ * Created on 13.12.16.
+ */
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Fork(1)
+@State(Scope.Thread)
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+public class BuilderInsertCharConcat {
+
+    private static final char SLASH = '/';
+    private static final char QUESTION_MARK = '?';
+
+    private String uri = "http://localhost?eventType=CRITICAL&from=0&to=1497437160327&limit=10&offset=0";
+
+    public static void main(String[] args) {
+        String uri = "http://localhost?eventType=CRITICAL&from=0&to=1497437160327&limit=10&offset=0";
+        int start = uri.indexOf("://");
+        int index = uri.indexOf(QUESTION_MARK, start + 3);
+        int len = uri.length();
+        String s = uri.substring(0, index) + SLASH + uri.substring(index, len);
+
+        System.out.println(s);
+    }
+
+    @Benchmark
+    public String original() {
+        int start = uri.indexOf("://");
+        int index = uri.indexOf(QUESTION_MARK, start + 3);
+        int len = uri.length();
+        StringBuilder sb = new StringBuilder(len + 1);
+        sb.append(uri, 0, index)
+                .append(SLASH)
+                .append(uri, index, len);
+        return sb.toString();
+    }
+
+    @Benchmark
+    public String improved() {
+        int start = uri.indexOf("://");
+        int index = uri.indexOf(QUESTION_MARK, start + 3);
+        int len = uri.length();
+        return new StringBuilder(len + 1)
+                .append(uri, 0, index)
+                .append(SLASH)
+                .append(uri, index, len)
+                .toString();
+    }
+
+    @Benchmark
+    public String insert() {
+        int start = uri.indexOf("://");
+        int index = uri.indexOf(QUESTION_MARK, start + 3);
+
+        return new StringBuilder(uri)
+                .insert(index, SLASH)
+                .toString();
+    }
+
+    @Benchmark
+    public String concat() {
+        int start = uri.indexOf("://");
+        int index = uri.indexOf(QUESTION_MARK, start + 3);
+        int len = uri.length();
+        return uri.substring(0, index) + SLASH + uri.substring(index, len);
+    }
+}
