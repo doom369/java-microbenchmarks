8b873c2f99b33985c320116305074852426f9d10
Dmitriy Dumanskiy <doom369@gmail.com>
2017-11-06 23:19:45 +0200

    wrong dependency removed

--- src/test/java/cc/microbenchmarks/core/reflection/setfield/ReflectionFieldAccess.java
+++ src/test/java/cc/microbenchmarks/core/reflection/setfield/ReflectionFieldAccess.java
@@ -1,12 +1,10 @@
 package cc.microbenchmarks.core.reflection.setfield;
 
-import com.sun.deploy.util.ReflectionUtil;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
