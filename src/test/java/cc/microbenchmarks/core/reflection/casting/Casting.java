9ac7b30f88fe3c506135c8683828b30d4792e639
Dmitriy Dumanskiy <doom369@gmail.com>
2017-11-06 23:06:03 +0200

    casting

---
+++ src/test/java/cc/microbenchmarks/core/reflection/casting/Casting.java
@@ -0,0 +1,48 @@
+package cc.microbenchmarks.core.reflection.casting;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.BenchmarkParams;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * The Blynk Project.
+ * Created by Dmitriy Dumanskiy.
+ * Created on 13.12.16.
+ */
+@BenchmarkMode(Mode.Throughput)
+@Fork(1)
+@State(Scope.Thread)
+@Warmup(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS, batchSize = 1000)
+@Measurement(iterations = 40, time = 1, timeUnit = TimeUnit.SECONDS, batchSize = 1000)
+public class Casting {
+
+    public Object msg;
+
+    public Class<String> type;
+
+    @Setup
+    public void setup(BenchmarkParams params) {
+        type = String.class;
+        msg = "123";
+    }
+
+    @Benchmark
+    public String castMethod() {
+        return type.cast(msg);
+    }
+
+    @Benchmark
+    public String explicitCasting() {
+        return (String) msg;
+    }
+
+}
